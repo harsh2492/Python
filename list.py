ed975dc09b7e248c1171d6bdfb03c524a82c407f
harsh2492 <herman.2492@gmail.com>
2023-08-05 17:50:55 +0530

    Add new list methods and debug messages
    1. Added reverse list method
    2. Added method to append a new list
    3. Added debug messages to the existing or new list methods

--- list.py
+++ list.py
@@ -7,25 +7,31 @@ class MyCustomList:
     print("Hello my custom list name is " + self.name)
 
   def addElementAtLast(self,element):
-  	#print("Adding following list element at the last : " + element)
+  	print("Adding element",element, "at the last position")
   	self.myList.append(element)
-  	#print(self.myList)
-  
+
   def addElementAtPos(self,index,element):
+  	print("Adding element",element, "at position ",index)
   	self.myList.insert(index-1,element)
   	
   def displayMyListElement(self):
-  	print("My list elements are: ")
-  	print(self.myList)
-  	
+  	print("My list elements are: ", self.myList)
+
   def reverseMyList(self):
   	print("Reversing my list")
   	self.myList.reverse()
   	
   def removeElementFromListAtPos(self,pos):
-  	print("Remvoing following element " + self.myList[pos-1])
+  	print("Remvoing following element " ,self.myList[pos-1])
   	self.myList.pop(pos-1)
   	
+  def printLenOfList(self):
+  	print("Length of my list is ",len(self.myList))
+  	
+  def appendNewListAtEnd(self,list):
+  	print("Adding following list",list,"at the end of my current list",self.myList)
+  	self.myList = self.myList + list
+
 p1 = MyCustomList("List1")
 p1.myListName()
 
@@ -42,4 +48,9 @@ p1.reverseMyList()
 p1.displayMyListElement()
 
 p1.removeElementFromListAtPos(2)
+p1.displayMyListElement()
+
+p1.printLenOfList()
+
+p1.appendNewListAtEnd(["Ten","Eleven"])
 p1.displayMyListElement()
